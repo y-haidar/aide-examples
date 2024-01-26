6073d0f9aaedc89f636664f6d032075d7b41edca
y-haidar <yousef.haidar@outlook.com>
2024-01-26 16:30:19 +0300

    more minimal example of the issue

--- examples/custom-apperror/src/main.rs
+++ examples/custom-apperror/src/main.rs
@@ -3,14 +3,14 @@ use std::sync::Arc;
 use aide::{axum::ApiRouter, openapi::OpenApi, transform::TransformOpenApi};
 use axum::Extension;
 use docs::docs_routes;
-use state::AppState;
-use todos::routes::todo_routes;
+// use state::AppState;
+// use todos::routes::todo_routes;
 
 mod docs;
-mod error;
-mod extractors;
-mod state;
-mod todos;
+// mod error;
+// mod extractors;
+// mod state;
+// mod todos;
 
 #[tokio::main]
 async fn main() {
@@ -19,14 +19,14 @@ async fn main() {
     });
     aide::gen::extract_schemas(true);
 
-    let state = AppState::default();
+    // let state = AppState::default();
     let mut api = OpenApi::default();
 
     let app = ApiRouter::new()
         // .nest_api_service("/todo", todo_routes().with_state(state))
         .nest_api_service("/docs", docs_routes())
-        .finish_api(&mut api)
-        // .finish_api_with(&mut api, api_docs)
+        // .finish_api(&mut api)
+        .finish_api_with(&mut api, api_docs)
         .layer(Extension(Arc::new(api)));
 
     println!("Example docs are accessible at http://127.0.0.1:3001/docs");
@@ -55,15 +55,15 @@ fn api_docs(api: TransformOpenApi) -> TransformOpenApi {
                 extensions: Default::default(),
             },
         )
-        .default_response_with::<axum::Json<error::AppError>, _>(|res| {
-            res.example(error::AppError {
-                error: "some error happened".to_string(),
-                error_details: None,
-                error_id: uuid::Uuid::nil(),
-                // This is not visible.
-                status: axum::http::StatusCode::IM_A_TEAPOT,
-            })
-        })
+    // .default_response_with::<axum::Json<error::AppError>, _>(|res| {
+    //     res.example(error::AppError {
+    //         error: "some error happened".to_string(),
+    //         error_details: None,
+    //         error_id: uuid::Uuid::nil(),
+    //         // This is not visible.
+    //         status: axum::http::StatusCode::IM_A_TEAPOT,
+    //     })
+    // })
     // .summary("summary")
     // .description("description")
 }
